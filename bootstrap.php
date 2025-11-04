b466ffd038ad291258a5e822350cd67d730cdb3f
ezequielcastelnuovo <eze.castelnuovo@gmail.com>
2025-11-04 11:06:15 -0300

    Honor X-Forwarded-Proto from ALB to avoid HTTPS redirect loops

--- bootstrap.php
+++ bootstrap.php
@@ -1,4 +1,8 @@
 <?php
+// Respeta el HTTPS cuando estamos detrás de un ALB que envía X-Forwarded-Proto
+if (!empty($_SERVER['HTTP_X_FORWARDED_PROTO']) && strtolower($_SERVER['HTTP_X_FORWARDED_PROTO']) === 'https') {
+    $_SERVER['HTTPS'] = 'on';
+}
 
 require "autoloader.php";
 require "error_handler.php";
